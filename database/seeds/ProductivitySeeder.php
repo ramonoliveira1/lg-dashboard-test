edc9a15e8cb3983ed805a5163f6e1c94e98bb198
Ramon Oliveira Novaes <ramon.ubt@gmail.com>
2026-03-20 20:55:46 -0300

    Initial commit

---
+++ database/seeds/ProductivitySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductivitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
+
