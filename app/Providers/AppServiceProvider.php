1514c8d4c46f6c10387c3a30625796a871da02a7
Ramon Oliveira Novaes <ramon.ubt@gmail.com>
2026-03-21 19:17:56 -0300

    feat: bind interfaces to implementations in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\ProductivityRepository;
+use App\Repositories\ProductivityRepositoryInterface;
+use App\Services\GeminiService;
+use App\Services\GeminiServiceInterface;
+use App\Services\ProductivityService;
+use App\Services\ProductivityServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +19,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            ProductivityRepositoryInterface::class,
+            ProductivityRepository::class
+        );
+
+        $this->app->bind(
+            ProductivityServiceInterface::class,
+            ProductivityService::class
+        );
+
+        $this->app->bind(
+            GeminiServiceInterface::class,
+            GeminiService::class
+        );
     }
 
     /**
